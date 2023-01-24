8cc4559fb2c3416f882063d1924590ad024b2884
Rahul Kathuria <rahulkathuria008@gmail.com>
2023-01-25 01:02:32 +0530

    get started button working

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+const scrollUp = () =>{
+  window.scrollTo({
+    top:0,
+    behavior: "smooth"
+  })
+}
 
 const Button = ({ styles }) => (
-  <a
-  href="/about"
+  <Link
+  to="/about"
   className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}
+  onClick={scrollUp}
 >
 Get Started
-</a>
+</Link>
  
 //  <button type="button" className={`py-4 px-6 font-poppins font-medium text-[18px] text-primary bg-blue-gradient rounded-[10px] outline-none ${styles}`}>
 //   Get Started
